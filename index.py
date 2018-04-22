0b24739395af02f6fa3b5499d6c0c418980f6b64
nanorocks <andrejnankov@gmail.com>
2018-04-22 22:43:40 +0200

    fix files again.

--- index.py
+++ index.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
     try:
+        URL = "https://www.pexels.com"
         s = webScanner.Scanner(URL)
     except:
         return render_template("index.html", dataFound="The web Scanner is sleeping, come back later.", pageTitle="Home page")
     gc.collect()
     pageTitle = s.getPageTitle()
     images = s.getSpecificData()
-
     return render_template("index.html", pageTitle=pageTitle, images=images)
 
 @app.route("/about", methods=['GET'])
@@ -23,15 +23,14 @@ def about():
 @app.route("/search/<word>", methods=['GET','POST'])
 def search(word=None):
     try:
+        URL = "https://www.pexels.com"
         s = webScanner.Scanner(URL + "/search/" + word)
     except:
         return render_template("index.html", dataFound="The web Scanner is sleeping, come back later.", pageTitle="Home page")
     gc.collect()
     pageTitle = s.getPageTitle()
     images = s.getSpecificData()
-    print(images)
     return render_template("index.html", pageTitle=pageTitle, images=images)
 
 if __name__ == '__main__':
-    URL = "https://www.pexels.com"
     app.run(debug=True, use_reloader=True)
